2b62aac0db4db7271be2f1432abfa7dd76ceebea
Mollydayne <cl.w.perez@gmail.com>
2024-11-27 14:07:18 +0100

    "Add of main.c main.h and _printf.c files for task 0"

---
+++ main.h
@@ -0,0 +1,28 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+* struct selector - Associate a format and the function
+* @format: Format specifier
+* @func: Pointer to the good function for the format
+*/
+typedef struct selector
+{
+char *format;
+int (*func)(char *format, va_list);
+} structype_t;
+
+int _printf(char *format, ...);
+int _putchar(char c);
+int print_c(va_list list);
+int print_str(va_list list);
+int print_percent(va_list list);
+
+#endif
